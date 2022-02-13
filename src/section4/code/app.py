cb0e4f7565229d9f27fba6e30f25fd59ffa53227
Foysal Ahamed <thisisfoysal@gmail.com>
2022-02-14 00:50:19 +0600

    Add jwt authentication

--- src/section4/code/app.py
+++ src/section4/code/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'ahamed'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with the name '{}' already exists.".format(name)}, 400
@@ -23,6 +31,7 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_required()
     def get(self):
         return {'items': items}
 
