0289e893b49400c1ba0195c1facb26e3899d1bb2
Foysal Ahamed <thisisfoysal@gmail.com>
2022-03-04 13:17:27 +0600

    Save item resource to database

--- src/section5/code/item.py
+++ src/section5/code/item.py
@@ -15,29 +15,29 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-
-        connection.close()
-
-        if row:
-            return {'item': {'name': row[0], 'price': row[1]}}
-
+        item = self.find_by_name(name)
+        if item:
+            return item
         return {'message': 'Item not found'}, 401
 
     @jwt_required()
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return {'message': "An item with the name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     @jwt_required()
@@ -58,6 +58,20 @@ class Item(Resource):
             item.update(data)
 
         return item
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
 
 
 class ItemList(Resource):
