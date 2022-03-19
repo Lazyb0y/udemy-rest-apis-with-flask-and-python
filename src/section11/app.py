98f07bc287d17a126e1180dd44c3d0b7f12aa312
Foysal Ahamed <thisisfoysal@gmail.com>
2022-03-19 20:38:52 +0600

    Fix a bug where User resource was not registered properly

--- src/section11/app.py
+++ src/section11/app.py
@@ -27,7 +27,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-api.add_resource(User, '/user<int:user_id>')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
