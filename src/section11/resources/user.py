466bf82b1bebf798861e38c1c793d7725f5947a0
Foysal Ahamed <thisisfoysal@gmail.com>
2022-03-20 20:25:52 +0600

    Add token refresh api

--- src/section11/resources/user.py
+++ src/section11/resources/user.py
@@ -1,4 +1,9 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity
+)
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
@@ -65,3 +70,12 @@ class UserLogin(Resource):
             }
 
         return {'message': 'Invalid credentials'}, 401
+
+
+class TokenRefresh(Resource):
+
+    @jwt_required(refresh=True)
+    def post(self):
+        user_id = get_jwt_identity()
+        new_token = create_access_token(identity=user_id, fresh=False)
+        return {'access_token': new_token}
